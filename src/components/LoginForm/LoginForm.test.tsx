33010ab35b19c3598c25cf32313e9ede84c92897
victorandujar <victorandujarmurcia@gmail.com>
2023-02-26 20:00:54 +0100

    Correct LoginForm test and LoginPage test

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,13 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import LoginForm from "./LoginForm";
 import { BrowserRouter } from "react-router-dom";
+import Wrapper from "../../mocks/Wrapper";
 
 describe("Given a LoginForm component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a button with the text 'Log in'", () => {
       const buttonText = "Log in";
 
-      render(<LoginForm />, { wrapper: BrowserRouter });
+      render(
+        <Wrapper>
+          <LoginForm />
+        </Wrapper>,
+        { wrapper: BrowserRouter }
+      );
 
       const expectedButton = screen.getByRole("button", { name: buttonText });
 
@@ -17,7 +23,12 @@ describe("Given a LoginForm component", () => {
     test("Then it should show a link with the text 'Sign up'", () => {
       const linkText = "Sign up";
 
-      render(<LoginForm />, { wrapper: BrowserRouter });
+      render(
+        <Wrapper>
+          <LoginForm />
+        </Wrapper>,
+        { wrapper: BrowserRouter }
+      );
 
       const expectedLink = screen.getByRole("link", { name: linkText });
 
